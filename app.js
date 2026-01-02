857f139e2dd5b23da204581d8b6cd0c320586e29
matthews jordao <matthewsjordao@gmail.com>
2026-01-02 09:44:40 -0500

    Fix CORS configuration to allow PATCH method for user updates

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const corsOptions = {
     'https://wtwr.bad.mn'
   ],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
